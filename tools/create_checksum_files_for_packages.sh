c3c7c90ec2f58dfaec2203b5c49944232c5ac4e0
Touchstone64 <touchstone64@gweb.me.uk>
2026-05-12 12:01:03 +0100

    Ensure scripts have a specified DMG path

--- tools/create_checksum_files_for_packages.sh
+++ tools/create_checksum_files_for_packages.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env zsh
 
-DMG_DIRECTORY=~/tmp/compile
+SCRIPT_NAME=${0:t}
+SCRIPT_DIR=${0:h}
+
+set -e
 
 function create_checksum_file_for_target {
     local target=$1
@@ -12,7 +15,22 @@ function create_checksum_file_for_target {
     echo "${checksum}  ${filename}" > ${outputfile}
 }
 
-for dmg in ${DMG_DIRECTORY}/*.dmg; do
+if [ $# -ne 1 ]; then
+    echo "usage: ${SCRIPT_NAME} <path>"
+    echo
+    echo "${SCRIPT_NAME} will create or replace SHA checksum files for any .dmg packages"
+    echo "found in <path>, using algorithms 1, 256 and 512, in the form <dmg-name>.sha<algorithm>.txt"
+    exit 1
+fi
+
+dmg_dir=${1}
+
+if [[ ! -d ${dmg_dir} ]]; then
+    echo "${SCRIPT_NAME}: path not found: ${dmg_dir}"
+    exit 2
+fi
+
+for dmg in ${dmg_dir}/*.dmg; do
     create_checksum_file_for_target ${dmg} 1
     create_checksum_file_for_target ${dmg} 256
     create_checksum_file_for_target ${dmg} 512
--- tools/remove_checksum_files_for_packages.sh
+++ tools/remove_checksum_files_for_packages.sh
@@ -1,8 +1,25 @@
 #! /usr/bin/env zsh
 
-DMG_DIRECTORY=~/tmp/compile
+SCRIPT_NAME=${0:t}
+SCRIPT_DIR=${0:h}
 
-for candidate in ${DMG_DIRECTORY}/*.dmg.sha*.txt; do
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "usage: ${SCRIPT_NAME} <path>"
+    echo
+    echo "${SCRIPT_NAME} will remove any files matching '*.dmg.sha*.txt' found in <path>."
+    exit 1
+fi
+
+dmg_dir=${1}
+
+if [[ ! -d ${dmg_dir} ]]; then
+    echo "${SCRIPT_NAME}: path not found: ${dmg_dir}"
+    exit 2
+fi
+
+for candidate in ${dmg_dir}/*.dmg.sha*.txt; do
     type=$(file -b ${candidate})
     if [[ ${type} != "ASCII text" ]]; then
         echo Not removing ${candidate:t} as it is not a text file
