e914198d924cf56d0521bb79dff1f1d804fbfdd0
Touchstone64 <touchstone64@gweb.me.uk>
2026-05-15 07:53:41 +0100

    Change the publish script to take architecture as an argument

--- publish_revision.sh
+++ publish_revision.sh
@@ -3,19 +3,27 @@
 SCRIPT_NAME=${0:t}
 SCRIPT_DIR=${0:h}
 
-if [ $# -ne 1 ]; then
-    echo "usage: ${SCRIPT_NAME} <release>, for example: ${SCRIPT_NAME} 98.0"
-    exit 1
+function print_usage_and_exit {
+    echo "usage: ${SCRIPT_NAME} <release> <arm64|x86_64>, for example: ${SCRIPT_NAME} 98.0 arm64"
+    exit $1
+}
+
+if [ $# -ne 2 ]; then
+    print_usage_and_exit 1 
 fi
 
 set -e
 
 RELEASE_VERSION="$1"
-RELEASE_TAG=release-${RELEASE_VERSION}
+ARCHITECTURE="$2"
+
+if [[ ${ARCHITECTURE} != "arm64" ]] && [[ ${ARCHITECTURE} != "x86_64" ]]; then
+    print_usage_and_exit 2
+fi
 
 NEXT_REVISION_DIR=${SCRIPT_DIR}/next-package-revision
-MACHINE=$(uname -m)
-DMG_REVISION_FILE=${NEXT_REVISION_DIR}/${RELEASE_TAG}-${MACHINE}.txt
+RELEASE_TAG=release-${RELEASE_VERSION}
+DMG_REVISION_FILE=${NEXT_REVISION_DIR}/${RELEASE_TAG}-${ARCHITECTURE}.txt
 
 if [[ ! -f ${DMG_REVISION_FILE} ]]; then
     echo ${SCRIPT_NAME}: ${DMG_REVISION_FILE} must exist before release ${RELEASE_VERSION} can be published
